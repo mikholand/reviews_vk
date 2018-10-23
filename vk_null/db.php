c42e1d04e19c488e035a69f4163066f452bbc365
Oleg Mikhno <mikholand@gmail.com>
2018-10-24 00:29:06 +0300

    Update db.php

--- vk_null/db.php
+++ vk_null/db.php
@@ -9,6 +9,9 @@
 
 			$dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
 			$db = new PDO($dsn, $params['user'], $params['password']);
+			
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 			return $db;
 		}
